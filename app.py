25ee250ca95b0c5afe7a53cb7b4de8283a9cf4c2
VilkaMini <alaunisvilius@gmail.com>
2022-05-09 15:21:19 +0300

    Fixed spacing

--- app.py
+++ app.py
@@ -6,7 +6,5 @@ app = Flask(__name__, static_url_path = "", static_folder = 'static/')
 def portfolio():
     return render_template('index.html')
 
-
-
 if __name__ == "__main__":
     app.run(debug=True)
