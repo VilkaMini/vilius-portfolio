1d3c8868f378cd2e70ddd91e00b623d53a0dde8c
Vilius <alaunisvilius@gmail.com>
2023-05-15 12:08:09 +0200

    App

--- app.py
+++ app.py
@@ -6,15 +6,5 @@ app = Flask(__name__, static_url_path = "", static_folder = 'static/')
 def portfolio():
     return render_template('chatgpt.html')
 
-"""
-@app.route("/gamedev", methods=["GET"])
-def gamedev():
-    return render_template('gamedev.html')
-
-@app.route("/datascientist", methods=["GET"])
-def datascientists():
-    return render_template('index.html')
-"""
-
 if __name__ == "__main__":
     app.run(debug=True)
