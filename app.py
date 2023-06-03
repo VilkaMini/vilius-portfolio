6982e1e90e40f162eb0bd10679559d9ba22f5555
Vilius <43584156+VilkaMini@users.noreply.github.com>
2023-06-04 01:04:51 +0200

    New pages added

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__, static_url_path = "", static_folder = 'static/')
 
 @app.route("/", methods=["GET"])
 def portfolio():
     return render_template('chatgpt.html')
+
+@app.route("/wildlandsinteractiveexperience/", methods=["GET"])
+def wildlandsexperience():
+    return render_template('wildlandsinteractiveexperience.html')
+
+@app.route("/terranova/", methods=["GET"])
+def terranova():
+    return render_template('terranova.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
