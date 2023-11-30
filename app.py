253b2cb0a1acfe622cdf4e83f56bb4357a61b477
Vilius <43584156+VilkaMini@users.noreply.github.com>
2023-11-30 15:27:37 +0100

    Updated with CV inside

--- app.py
+++ app.py
@@ -22,6 +22,10 @@ def wordweaver():
 def aboutme():
     return render_template('aboutme.html')
 
+@app.route("/cv/", methods=["GET"])
+def cv():
+    return render_template('cv.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
